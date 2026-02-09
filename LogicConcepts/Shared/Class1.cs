28100c72213f93f8010eb6e9968ebd64b1c9603c
Yeidy Lopera <yeidylopera9@gmail.com>
2026-02-09 18:26:44 -0500

    The greathest number

---
+++ LogicConcepts/Shared/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace Shared;
+
+public static class ConsoleExtension
+{
+    public static int GetInt(string message)
+    {
+        Console.Write(message);
+        var numberString = Console.ReadLine();
+        if (int.TryParse(numberString, out int numberInt))
+        {
+            return numberInt;
+        }
+
+        return 0;
+    }
+
+    public static string? GetValidOptions(string message, List<string> options)
+    {
+        Console.Write(message);
+        var answer = Console.ReadLine();
+        if (options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)))
+        {
+            return answer;
+        }
+        return null;
+    }
+}
---
+++ LogicConcepts/TheGreathestNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    Console.WriteLine("Ingrese 3 numeros diferentes");
+    var a = ConsoleExtension.GetInt("Ingrese primer numero  : ");
+    var b = ConsoleExtension.GetInt("Ingrese segundo numero : ");
+    var c = ConsoleExtension.GetInt("Ingrese tercer numero  : ");
+
+    if (a > b && a > c)
+    {
+        Console.WriteLine($"El numeri mayor es: {a}");
+    }
+    else if (b > a && b > c)
+    {
+        Console.WriteLine($"El numero mayor es:{b}");
+    }
+    else
+    {
+        Console.WriteLine($"El numero mayor es:{c}");
+    }
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]i, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over. ");
+
+
+
