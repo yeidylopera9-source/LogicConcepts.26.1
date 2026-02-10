d3d9a86d368016df8a6433460705b6d527b057f0
Yeidy Lopera <yeidylopera9@gmail.com>
2026-02-09 20:27:18 -0500

    Is multiple of another

---
+++ LogicConcepts/IsMultipleOfAnother/Program.cs
@@ -0,0 +1,17 @@
+﻿using Shared;
+
+do
+{
+    var a = ConsoleExtension.GetInt("Ingrese primer numero  : ");
+    var b = ConsoleExtension.GetInt("Ingrese segundo numero : ");
+    if (b % a == 0)
+
+    {
+        Console.WriteLine($"{a} es multiplo de {b}");
+    }
+    else
+    {
+        Console.WriteLine($"{a} no es multiplo de {b}");
+    }
+
+}  while (true);
