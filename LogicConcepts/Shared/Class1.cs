367e7fa1cf64ed1348b9e1d8cb4033bf65f80980
Yeidy Lopera <yeidylopera9@gmail.com>
2026-02-16 18:10:11 -0500

    Shipment of goods

--- LogicConcepts/Shared/Class1.cs
+++ LogicConcepts/Shared/Class1.cs
@@ -43,7 +43,7 @@ public static class ConsoleExtension
         var text = Console.ReadLine();
         return text;
     }
-
+  
 
     public static string? GetValidOptions(string message, List<string> options)
     {
---
+++ LogicConcepts/ShipmentOfGoods/Program.cs
@@ -0,0 +1,97 @@
+﻿using Shared;
+using System.Reflection.Metadata.Ecma335;
+
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var weigth = ConsoleExtension.GetFloat("Peso de la mercancia ............:");
+    var value = ConsoleExtension.GetDecimal("Valor de la mercancia ...........:");
+    string isMonday;
+
+    do
+    {
+        isMonday = ConsoleExtension.GetValidOptions("Es lunes .[S]i, [N]o?............:", options)!;
+    } while (!options.Any(x => x.Equals(isMonday, StringComparison.CurrentCultureIgnoreCase)));
+
+    var payMethods = new List<string> { "e", "t" };
+    string payMethod;
+    do
+    {
+        payMethod = ConsoleExtension.GetValidOptions("Tipo de pago [E]fectivo [T]arjeta: ", payMethods)!;
+    } while (!payMethods.Any(x => x.Equals(payMethod, StringComparison.CurrentCultureIgnoreCase)));
+
+    var fare = CalculateFare(weigth);
+    var discount = CalculateDiscount(fare, value);
+    decimal promotion = 0;
+    if (discount == 0)
+    {
+        promotion = CalculatePromotion(fare, isMonday, payMethod, value);
+    }
+
+
+    Console.WriteLine($"Tarifa ..........................: {fare,20:C2}");
+    Console.WriteLine($"Descuento .......................: {discount,20:C2}");
+    Console.WriteLine($"Promocion .......................: {promotion,20:C2}");
+    Console.WriteLine($"Total a pagar ...................: {fare - discount - promotion,20:C2}");
+
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]i, [N]o?....: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+
+decimal CalculatePromotion(decimal fare, string isMonday, string payMethod, decimal value)
+{
+    if (isMonday.ToLower() == "s" && payMethod.ToLower() == "t")
+    {
+        return fare * 0.5m;
+    }
+    if (payMethod.ToLower() == "e" && value > 1000000m)
+    {
+        return fare * 0.4m;
+    }
+    return 0;
+}
+
+decimal CalculateDiscount(decimal fare, decimal value)
+{
+    if (value >= 300000m && value <= 600000m)
+    {
+        return fare * 0.1m;
+    }
+    if (value >= 600000m && value <= 1000000m)
+    {
+        return fare * 0.2m;
+    }
+    if (value >= 1000000m)
+    {
+        return fare * 0.3m;
+    }
+    return 0;
+}
+
+decimal CalculateFare(float weigth)
+{
+    if (weigth <= 100)
+    {
+        return 20000m;
+    }
+    if (weigth <= 150)
+    {
+        return 25000m;
+    }
+    if (weigth <= 200)
+    {
+        return 30000m;
+    }
+    int aditional = ((int)weigth - 200) / 10;
+    return 35000m + aditional * 2000m;
+
+}
+
